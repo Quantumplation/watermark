0b586ba103c6adfc0520de3977e714464affe193
Pi Lanningham <pi.lanningham@gmail.com>
2019-08-11 22:36:15 -0400

    Improve benchmarks
    Benchmarks were broken/misleading, and we now compare to hashset

--- README.md
+++ README.md
@@ -35,3 +35,41 @@ Internally, a watermarking set contains a "watermark" and a bitvector of
 that number have been seen, and the recently added items handles everything
 else.  This means that if all elements eventually get added, memory usage
 is kept very low and membership tests are very very cheap.
+
+## Performance
+
+This crate comes with some simple benchmarks that you can run for yourself.
+Here are the results, compared to a hash set, that I got on my laptop:
+
+```
+WatermarkSet Insert/In Order
+                        time:   [12.528 us 13.191 us 13.863 us]
+                        thrpt:  [72.135 Melem/s 75.807 Melem/s 79.819 Melem/s]
+
+HashSet Insert/In Order time:   [52.123 us 52.573 us 53.071 us]
+                        thrpt:  [18.843 Melem/s 19.021 Melem/s 19.186 Melem/s]
+
+WatermarkSet Insert/Out of Order
+                        time:   [20.463 us 21.415 us 22.209 us]
+                        thrpt:  [45.027 Melem/s 46.696 Melem/s 48.870 Melem/s]
+
+HashSet Insert/Out of Order
+                        time:   [35.186 us 35.563 us 36.051 us]
+                        thrpt:  [27.738 Melem/s 28.119 Melem/s 28.420 Melem/s]
+
+WatermarkSet Contains/Aligned
+                        time:   [482.96 ns 492.02 ns 502.25 ns]
+                        thrpt:  [1.2743 Gelem/s 1.3008 Gelem/s 1.3252 Gelem/s]
+
+HashSet Contains/Aligned
+                        time:   [11.687 us 11.943 us 12.255 us]
+                        thrpt:  [52.223 Melem/s 53.589 Melem/s 54.761 Melem/s]
+
+WatermarkSet Contains/Unaligned
+                        time:   [621.99 ns 674.32 ns 726.55 ns]
+                        thrpt:  [880.87 Melem/s 949.11 Melem/s 1.0290 Gelem/s]
+
+HashSet Contains/Unaligned
+                        time:   [13.132 us 13.431 us 13.801 us]
+                        thrpt:  [46.373 Melem/s 47.651 Melem/s 48.737 Melem/s]
+```
--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,12 +3,13 @@
 extern crate criterion;
 use criterion::{Criterion, Throughput, Benchmark, BatchSize};
 use watermark::WatermarkSet;
+use std::collections::HashSet;
 
 fn benchmarks(c: &mut Criterion) {
     let batch_size = 1000;
     let mut idx = 0;
     c.bench(
-        "Insert",
+        "WatermarkSet Insert",
         Benchmark::new(
             "In Order",
             move |b| {
@@ -16,7 +17,7 @@ fn benchmarks(c: &mut Criterion) {
                     WatermarkSet::default,
                     move |mut coll| {
                         for i in 1..batch_size {
-                            coll.insert(i + 2*idx);
+                            coll.insert(i + idx);
                         }
                         idx += batch_size;
                     },
@@ -26,8 +27,29 @@ fn benchmarks(c: &mut Criterion) {
         ).throughput(Throughput::Elements(batch_size)),
     );
 
+    idx = 0;
     c.bench(
-        "Insert",
+        "HashSet Insert",
+        Benchmark::new(
+            "In Order",
+            move |b| {
+                b.iter_batched(
+                    HashSet::new,
+                    move |mut coll| {
+                        for i in 1..batch_size {
+                            coll.insert(i + idx);
+                        }
+                        idx += batch_size;
+                    },
+                    BatchSize::SmallInput
+                );
+            }
+        ).throughput(Throughput::Elements(batch_size)),
+    );
+
+    idx = 0;
+    c.bench(
+        "WatermarkSet Insert",
         Benchmark::new(
             "Out of Order",
             move |b| {
@@ -48,8 +70,31 @@ fn benchmarks(c: &mut Criterion) {
         ).throughput(Throughput::Elements(batch_size)),
     );
 
+    idx = 0;
     c.bench(
-        "Contains",
+        "HashSet Insert",
+        Benchmark::new(
+            "Out of Order",
+            move |b| {
+                b.iter_batched(
+                    HashSet::new,
+                    move |mut coll| {
+                        for i in 1..(batch_size / 2) {
+                            coll.insert(i + 2*idx);
+                        }
+                        for i in 1..(batch_size / 2) {
+                            coll.insert(i + 2*idx + 1);
+                        }
+                        idx += batch_size;
+                    },
+                    BatchSize::SmallInput
+                );
+            }
+        ).throughput(Throughput::Elements(batch_size)),
+    );
+
+    c.bench(
+        "WatermarkSet Contains",
         Benchmark::new(
             "Aligned",
             move |b| {
@@ -73,7 +118,31 @@ fn benchmarks(c: &mut Criterion) {
     );
 
     c.bench(
-        "Contains",
+        "HashSet Contains",
+        Benchmark::new(
+            "Aligned",
+            move |b| {
+                b.iter_batched(
+                    || {
+                        let mut coll = HashSet::new();
+                        for i in 0..64*10 {
+                            coll.insert(i);
+                        }
+                        coll
+                    },
+                    |coll| {
+                        for i in 0..64*10 {
+                            coll.contains(&i);
+                        }
+                    },
+                    BatchSize::SmallInput
+                );
+            }
+        ).throughput(Throughput::Elements(64 * 10)),
+    );
+
+    c.bench(
+        "WatermarkSet Contains",
         Benchmark::new(
             "Unaligned",
             move |b| {
@@ -88,6 +157,30 @@ fn benchmarks(c: &mut Criterion) {
                     |coll| {
                         for i in 0..64*10 {
                             coll.contains(i);
+                        }
+                    },
+                    BatchSize::SmallInput
+                );
+            }
+        ).throughput(Throughput::Elements(64*10)),
+    );
+
+    c.bench(
+        "HashSet Contains",
+        Benchmark::new(
+            "Unaligned",
+            move |b| {
+                b.iter_batched(
+                    || {
+                        let mut coll = HashSet::new();
+                        for i in 0..64*5 {
+                            coll.insert(i * 2);
+                        }
+                        coll
+                    },
+                    |coll| {
+                        for i in 0..64*10 {
+                            coll.contains(&i);
                         }
                     },
                     BatchSize::SmallInput
